a93f81979bca3c66410f5883d8ce0452b753f863
personal PC <machenyu96@gmail.com>
2023-09-12 15:39:50 +0100

    fix error with global search

--- main.m
+++ main.m
@@ -4,6 +4,13 @@
 % subfolderName = '2_dim_data';
 % cd(subfolderName);
 
+% 
+% tic
+% filename = "data_10";
+% get_2step(10,filename);
+% dim2_handle(filename,'result_10_fmincon_25_new',5);
+% elapsed_time_10 = toc;
+% fprintf('Time taken for zero block: %f seconds\n', elapsed_time_10);
 
 
 tic
@@ -21,7 +28,7 @@ filename = "data_2000";
 get_2step(2000,filename);
 dim2_handle(filename,'result_2000_fmincon_25_new',5);
 elapsed_time_2000 = toc;
-fprintf('Time taken for second block: %f seconds\n', elapsed_time_2000);
+fprintf('Time taken for first block: %f seconds\n', elapsed_time_2000);
 
 
 
@@ -30,46 +37,46 @@ filename = "data_1000";
 get_2step(1000,filename);
 dim2_handle(filename,'result_1000_fmincon_400_new',20);
 elapsed_time_1000 = toc;
-fprintf('Time taken for first block: %f seconds\n', elapsed_time_1000);
+fprintf('Time taken for second block: %f seconds\n', elapsed_time_1000);
 
-% tic
-% filename = "data_2000";
-% get_2step(2000,filename);
-% dim2_handle(filename,'result_2000_fmincon_400_new',20);
-% elapsed_time_2000 = toc;
-% fprintf('Time taken for third block: %f seconds\n', elapsed_time_2000);
-% 
-% % 2nd
-% 
-% tic
-% filename = "data_1000_2nd";
-% get_2step_2nd(1000,filename);
-% dim2_handle(filename,'result_1000_fmincon_25_2nd_new',5);
-% elapsed_time_1000 = toc;
-% fprintf('Time taken for fourth block: %f seconds\n', elapsed_time_1000);
-% 
-% tic
-% filename = "data_1000_2nd";
-% get_2step_2nd(1000,filename);
-% dim2_handle(filename,'result_1000_fmincon_400_2nd_new',20);
-% elapsed_time_1000 = toc;
-% fprintf('Time taken for fivth block: %f seconds\n', elapsed_time_1000);
-% 
-% tic
-% filename = "data_2000_2nd";
-% get_2step_2nd(2000,filename);
-% dim2_handle(filename,'result_2000_fmincon_25_2nd_new',5);
-% elapsed_time_2000 = toc;
-% fprintf('Time taken for sixth block: %f seconds\n', elapsed_time_2000);
-% 
-% 
-% tic
-% filename = "data_2000_2nd";
-% get_2step_2nd(2000,filename);
-% dim2_handle(filename,'result_2000_fmincon_400_2nd_new',20);
-% elapsed_time_2000 = toc;
-% fprintf('Time taken for seventh block: %f seconds\n', elapsed_time_2000);
-% 
+tic
+filename = "data_2000";
+get_2step(2000,filename);
+dim2_handle(filename,'result_2000_fmincon_400_new',20);
+elapsed_time_2000 = toc;
+fprintf('Time taken for third block: %f seconds\n', elapsed_time_2000);
+
+% 2nd
+
+tic
+filename = "data_1000_2nd";
+get_2step_2nd(1000,filename);
+dim2_handle(filename,'result_1000_fmincon_25_2nd_new',5);
+elapsed_time_1000 = toc;
+fprintf('Time taken for fourth block: %f seconds\n', elapsed_time_1000);
+
+tic
+filename = "data_1000_2nd";
+get_2step_2nd(1000,filename);
+dim2_handle(filename,'result_1000_fmincon_400_2nd_new',20);
+elapsed_time_1000 = toc;
+fprintf('Time taken for fivth block: %f seconds\n', elapsed_time_1000);
+
+tic
+filename = "data_2000_2nd";
+get_2step_2nd(2000,filename);
+dim2_handle(filename,'result_2000_fmincon_25_2nd_new',5);
+elapsed_time_2000 = toc;
+fprintf('Time taken for sixth block: %f seconds\n', elapsed_time_2000);
+
+
+tic
+filename = "data_2000_2nd";
+get_2step_2nd(2000,filename);
+dim2_handle(filename,'result_2000_fmincon_400_2nd_new',20);
+elapsed_time_2000 = toc;
+fprintf('Time taken for seventh block: %f seconds\n', elapsed_time_2000);
+
 
 
 disp('Everything is done.');
--- optimize_function.m
+++ optimize_function.m
@@ -1,40 +1,70 @@
+% range_X is the initial region
+% range_Y is the target region
+
+% function [min_value, max_value] = optimize_function(X, Y, range_X, H, h, range_Y,all_target_regions)
 function [min_value, max_value] = optimize_function(X, Y, range_X, H, h, range_Y)
+
+    % Nonlinear constraints
+%     nonlcon = @(x) prob_constraints(x,X,Y,all_target_regions,H,h);
+
+    % Define the objective function for minimization
+    fun_min = @(x) target_function(x, X, Y, range_X, H, h);
+
+    % Define the objective function for maximization (by negating the original function)
+    fun_max = @(x) -target_function(x, X, Y, range_X, H, h);
+
+   
 
     % Set the bounds
     lb = [range_X(1); range_X(2)]; %[x_min;y_min]
     ub = [range_X(3); range_X(4)]; %[x_max;y_max]
 
-    % Define the objective function for minimization
-    fun_min = @(x) target_function(x, X, Y, range_X, H, h);
-    
-    % Define the objective function for maximization (by negating the original function)
-    fun_max = @(x) -target_function(x, X, Y, range_X, H, h);
+    % Initial guess for x
+    x0 = (lb+ub)/2;
 
     % Optimization options
     options = optimoptions('fmincon','Display','off');
-    
-
-
-    % Choose a random initial guess within the bounds
-
-    x0 = (lb+ub)/2;
 
     % Use fmincon to find the minimum value
     x_min = fmincon(fun_min, x0, [], [], [], [], lb, ub, [], options);
-    min_value = fun_min(x_min);
-
+    min_value = target_function(x_min, X, Y, range_Y, H, h);
 
     % Use fmincon to find the maximum value (by searching for the minimum of the negated function)
     x_max = fmincon(fun_max, x0, [], [], [], [], lb, ub, [], options);
-    max_value = -fun_max(x_max);
+    max_value = target_function(x_max, X, Y, range_Y, H, h);
+
+    if min_value>max_value
+        disp("error");
+        % Define the optimization problem
+        problem_min = createOptimProblem('fmincon', 'objective', fun_min, ...
+            'x0', (lb+ub)/2, 'lb', lb, 'ub', ub);
+        
+        problem_max = createOptimProblem('fmincon', 'objective', fun_max, ...
+            'x0', (lb+ub)/2, 'lb', lb, 'ub', ub);
+    
+        % Create a GlobalSearch object
+        gs = GlobalSearch('Display', 'off');
+    
+        % Run GlobalSearch
+        [x_min, min_value] = run(gs, problem_min);
+        [~, max_neg_value] = run(gs, problem_max);
+    
+        max_value = -max_neg_value;
+        if min_value>max_value
+            disp("error!!!");
+        end  
+
+    end
+    
+    
+
 
 end
-
 
 function val = target_function(x, X, Y, range_Y, H, h)
     % Get the integral value
     val = integra_nonpara(x, X, Y, range_Y, H, h);
-    
+
     % Combine with normcdf (based on your requirement)
 %     val = normcdf(int_val); 
 end
