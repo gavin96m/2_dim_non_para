929736b35625c9353b5cd83af7abff1cf652f1e2
personal PC <machenyu96@gmail.com>
2023-09-16 08:50:50 +0100

    update parfor

--- dim2_handle_by_point.m
+++ dim2_handle_by_point.m
@@ -67,51 +67,54 @@ function dim2_handle_by_point(filename,datafilename,regionsize,numpoint)
 
 
     total_steps = rows * cols;
-
-    for i = 1:rows
-        for j = 1:cols
-
-            % Initialize the temporary probability storage for this source region
-            temp_probability = inf(num_regions, 2); 
-            temp_probability(:, 2) = -inf; % Set second column (max value) to negative infinity for initialization
-            
-            % lower left corner
-            x_start = x_min + (i - 1) * x_step;
-            y_start = y_min + (j - 1) * y_step;
-            
-            % upper right corner
-            x_end = x_start + x_step;
-            y_end = y_start + y_step;
-            
-            num_points = numpoint;    
-            x_points = linspace(x_start, x_end, num_points);
-            y_points = linspace(y_start, y_end, num_points);
-            
-           for k = 1:num_points
-                for l = 1:num_points
-                    x0 = [x_points(k);y_points(l)];
     
-                    for m = 1:num_regions
-                        range_Y = regions(m,:);
-                        int_y = integra_nonpara(x0,init_data,next_data,range_Y,h_next,h_init);
+    parfor_progress(rows*cols);
+    probability = zeros(rows*cols, num_regions, 2); % Preallocate for efficiency
+    parfor idx = 1:(rows*cols) % Combined loop counter for i and j
+        
+        % Compute i and j from the index
+        i = mod(idx-1, rows) + 1;
+        j = ceil(idx / rows);
+        
+        % Initialize the temporary probability storage for this source region
+        temp_probability = inf(num_regions, 2); 
+        temp_probability(:, 2) = -inf; % Set second column (max value) to negative infinity for initialization
+        
+        % lower left corner
+        x_start = x_min + (i - 1) * x_step;
+        y_start = y_min + (j - 1) * y_step;
+        
+        % upper right corner
+        x_end = x_start + x_step;
+        y_end = y_start + y_step;
+        
+        num_points = numpoint;    
+        x_points = linspace(x_start, x_end, num_points);
+        y_points = linspace(y_start, y_end, num_points);
+        
+        for k = 1:num_points
+            for l = 1:num_points
+                x0 = [x_points(k);y_points(l)];
     
-                        % Update the minimum probability
-                        temp_probability(m, 1) = min(temp_probability(m, 1), int_y);
+                for m = 1:num_regions
+                    range_Y = regions(m,:);
+                    int_y = integra_nonpara(x0,init_data,next_data,range_Y,h_next,h_init);
     
-                        % Update the maximum probability
-                        temp_probability(m, 2) = max(temp_probability(m, 2), int_y);
-                    end
+                    % Update the minimum probability
+                    temp_probability(m, 1) = min(temp_probability(m, 1), int_y);
+    
+                    % Update the maximum probability
+                    temp_probability(m, 2) = max(temp_probability(m, 2), int_y);
                 end
             end
-            
-            % After going through all the points in the source region, store the probabilities
-            probability((j-1)*rows + i, :, :) = temp_probability; 
-            % print progress
-            current_step = (i-1)*cols + j;
-            progress = current_step / total_steps * 100;
-            fprintf('Progress: %.2f%%\n', progress)
         end
+        
+        probability(idx,:,:) = temp_probability;
+        parfor_progress;
     end
+
+
+
     assignin('base','probability',probability);
 
 %     file_name = 'result_prob.csv';
--- main.m
+++ main.m
@@ -2,55 +2,28 @@
 % x,y [0-2]
 
 
+
+
 tic
-filename = "data_1000";
-get_2step(1000,filename);
-dim2_handle(filename,'result_1000_by_point_25_100',5);
-elapsed_time_1000 = toc;
-fprintf('Time taken for first block: %f seconds\n', elapsed_time_1000);
+filename = "data_2000";
+get_2step(2000,filename);
+dim2_handle_by_point(filename,'result_2000_by_point_400_100',20,10);
+elapsed_time_2000 = toc;
+fprintf('Time taken for first block: %f seconds\n', elapsed_time_2000);
 
+tic
+filename = "data_2000_a2";
+get_2step_2nd(2000,filename);
+dim2_handle_by_point(filename,'result_2000_a2_by_point_25_100',5,10);
+elapsed_time_2000 = toc;
+fprintf('Time taken for second block: %f seconds\n', elapsed_time_2000);
 
-% tic
-% filename = "data_1000";
-% get_2step(1000,filename);
-% dim2_handle_by_point(filename,'result_1000_by_point_25_100',5,10);
-% elapsed_time_1000 = toc;
-% fprintf('Time taken for first block: %f seconds\n', elapsed_time_1000);
-% 
-% tic
-% filename = "data_1000";
-% get_2step(1000,filename);
-% dim2_handle_by_point(filename,'result_1000_by_point_25_400',5,20);
-% elapsed_time_1000 = toc;
-% fprintf('Time taken for second block: %f seconds\n', elapsed_time_1000);
-% 
-% tic
-% filename = "data_2000";
-% get_2step(2000,filename);
-% dim2_handle_by_point(filename,'result_2000_by_point_25_100',5,10);
-% elapsed_time_2000 = toc;
-% fprintf('Time taken for third block: %f seconds\n', elapsed_time_2000);
-% 
-% tic
-% filename = "data_2000";
-% get_2step(2000,filename);
-% dim2_handle_by_point(filename,'result_2000_by_point_25_400',5,20);
-% elapsed_time_2000 = toc;
-% fprintf('Time taken for fourth block: %f seconds\n', elapsed_time_2000);
-% 
-% tic
-% filename = "data_1000";
-% get_2step(1000,filename);
-% dim2_handle_by_point(filename,'result_1000_by_point_400_400',20,20);
-% elapsed_time_1000 = toc;
-% fprintf('Time taken for fivth block: %f seconds\n', elapsed_time_1000);
-% 
-% tic
-% filename = "data_2000";
-% get_2step(2000,filename);
-% dim2_handle_by_point(filename,'result_2000_by_point_400_400',20,20);
-% elapsed_time_2000 = toc;
-% fprintf('Time taken for sixth block: %f seconds\n', elapsed_time_2000);
+tic
+filename = "data_2000_a2";
+get_2step_2nd(2000,filename);
+dim2_handle_by_point(filename,'result_2000_a2_by_point_400_100',20,10);
+elapsed_time_2000 = toc;
+fprintf('Time taken for third block: %f seconds\n', elapsed_time_2000);
 
 
 disp('Everything is done.');
